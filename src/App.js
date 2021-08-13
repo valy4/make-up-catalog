03e93273b3213d95213c63c2f0b74a805b168dbb
Daniel Ghimpu <smeu.valentina33@gmail.com>
2021-08-13 15:24:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import react, { useState, useEffect } from "react"
 
 function App() {
+  const [list, setList] = useState([])
+  const [nrPages, setNrPages] = useState([])
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline&page=${currentPage}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setList(data)
+        const total = Math.ceil(data.length / 10)
+        setNrPages(total)
+
+      })
+  }, [currentPage])
+  const pagesList = []
+  for (let i = 1; i <= nrPages; i++)
+    pagesList.push(i)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>hello</p>
+      {list.map(function (item) {
+        return (
+          <div>
+            <p>{item.id}</p>
+            <img src={item.image_link}></img>
+          </div>
+        )
+      })}
+      {pagesList.map(function (page) {
+        return (
+          <button
+            onClick={() => setCurrentPage(page)}>{page}</button>
+
+        )
+      })}
+
     </div>
   );
 }
